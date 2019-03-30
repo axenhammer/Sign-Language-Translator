e3aaa90ae03cf88ebb4524580f91c28463b6dccb
KrishnaAlagiri <krishna.alagiri03@gmail.com>
2019-03-31 04:16:23 +0530

    HSV instead instead of RGB Range Mapping

--- tohand.py
+++ tohand.py
@@ -1,20 +1,48 @@
 import numpy as np
 import cv2
-boundary = [([0, 120, 0], [140, 255, 100]),([25, 0, 75], [180, 38, 255])]
 
+"""
+# Red and Green based on the dataset
+boundary = [
+            ([0, 120, 0], [140, 255, 100]),
+            ([25, 0, 75], [180, 38, 255])
+           ]
+
+# Skin colour (maha)
+boundary =  [
+            ([115,67,49], [237,206,187]),
+            ([180,157,139], [210,202,189])
+            ]
+
+# Skin colour (KK)
+boundary =  [
+            ([0,0,0],[147,120,120]),
+            ([180,157,139], [210,202,189])
+            ]
+# Skin colour (Ajay)
+boundary =  [
+            ([129,117,104],[200,160,125]),
+            ([0,0,0], [140,90,73])
+            ]
+"""
+lower = np.array([0, 45, 80], dtype = "uint8")
+upper = np.array([20, 255, 255], dtype = "uint8")
 
 def handsegment(frame):
-    l, u = boundary[0]
-    l = np.array(l, dtype="uint8")
-    u = np.array(u, dtype="uint8")
+    #l, u = boundary[1]
+    l = lower
+    u = upper
     mask1 = cv2.inRange(frame, l, u)
-
+    """
     l, u = boundary[1]
     l = np.array(l, dtype="uint8")
     u= np.array(u, dtype="uint8")
     mask2 = cv2.inRange(frame, l, u)
     mask = cv2.bitwise_or(mask1, mask2)
-    output = cv2.bitwise_and(frame, frame, mask=mask)
+    """
+    converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    skinMask = cv2.inRange(converted, lower, upper)
+    output = cv2.bitwise_and(frame, frame, mask=skinMask)
     return output
 
 if __name__ == '__main__':
