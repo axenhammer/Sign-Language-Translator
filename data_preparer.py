480466ec44e03cb1ecc6c9309a7bd3a45d15b8db
Maha V <40058339+maha2000@users.noreply.github.com>
2019-03-31 16:34:38 +0530

    Added two modules for image prediction

---
+++ data_preparer.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+import cv2
+import pickle
+
+DATADIR="training_data/classes"
+CATEGORIES = ["Everyone","Good_evening"]
+training_data =[]
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR,category)
+        class_num =CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path,img),0)
+                training_data.append([img_array ,class_num])
+            except Exception as e:
+                pass
+create_training_data()
+
+X = []
+y = []
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+X=np.array(X).reshape(-1,1920,1080,1)
+
+
+pickle_out=open("X.pickle","wb")
+pickle.dump(X,pickle_out)
+pickle_out.close()
+
+pickle_out=open("y.pickle","wb")
+pickle.dump(y,pickle_out)
+pickle_out.close()
---
+++ model.py
@@ -0,0 +1,38 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+# more info on callbakcs: https://keras.io/callbacks/ model saver is cool too.
+from tensorflow.keras.callbacks import TensorBoard
+from keras.utils import to_categorical
+import pickle
+import time
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+print(X.shape)
+
+NAME = 'Motion Detection model'
+print(NAME)
+
+model = Sequential()
+model.add(LSTM(,return_sequences=False,
+               input_shape=(60,1920,1080,1)
+               dropout=0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(, activation='softmax'))
+
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+model.fit(X, y,batch_size=60,epochs=5,validation_split=0.2,callbacks=[tensorboard])
+
+
+model.save('motion_detection.model')
+
+
+
+
