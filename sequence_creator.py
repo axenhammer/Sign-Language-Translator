e10bc1be063820919cd34ed24372ced79778c9f1
Maha V <40058339+maha2000@users.noreply.github.com>
2019-03-31 15:07:35 +0530

    Added a sequence creator

---
+++ sequence_creator.py
@@ -0,0 +1,20 @@
+import numpy as np
+import os
+import cv2
+
+def sequence_creator(user_path)
+	path=user_path
+	x=[]
+	for i in os.listdir(path):
+	    x.append(cv2.imread(path+i,0))
+	p = x[0]
+	k=1
+	for i in x:
+	    p[np.where(i> 190)]=0 + (255/60)*k
+	    p += i
+	    k+=1
+	cv2.imshow("hello",p)
+	cv2.waitKey(0)
+	path ='training_data/everybody/'
+	print(path+'everyone_1')
+	cv2.imwrite( path+'everyone_1.jpg' ,p)
