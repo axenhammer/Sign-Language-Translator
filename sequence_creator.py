1c7048e7a627af6afff5dcdecdef5896155323ba
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2019-04-01 10:07:45 +0530

    minor

--- sequence_creator.py
+++ sequence_creator.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 import numpy as np
 import os
 import cv2
 
-def sequence_creator(user_path)
-	path=user_path
+def sequence_creator(user_path, final_path)
+	path = user_path
 	x=[]
 	for i in os.listdir(path):
 	    x.append(cv2.imread(path+i,0))
@@ -12,7 +13,7 @@ def sequence_creator(user_path)
 	for i in x:
 	    p[np.where(i> 190)]=0 + (255/60)*k
 	    p += i
-	    k+=1
+	    k += 1
 	cv2.imshow("hello",p)
 	cv2.waitKey(0)
 	path ='training_data/everybody/'
