637b7ab74122a1a7f9356b8fcbfb89ced17b7828
KrishnaAlagiri <krishna.alagiri03@gmail.com>
2019-04-01 20:32:37 +0530

    Final

--- temp/threshold.py
+++ temp/threshold.py
@@ -3,32 +3,6 @@ import numpy as np
 import copy
 import math
 
-def calculateFingers(res, drawing):
-    #  convexity defect
-    hull = cv2.convexHull(res, returnPoints=False)
-    if len(hull) > 3:
-        defects = cv2.convexityDefects(res, hull)
-        if defects is not None:
-            cnt = 0
-            for i in range(defects.shape[0]):  # calculate the angle
-                s, e, f, d = defects[i][0]
-                start = tuple(res[s][0])
-                end = tuple(res[e][0])
-                far = tuple(res[f][0])
-                a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
-                if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
-                    cnt += 1
-                    cv2.circle(drawing, far, 8, [211, 84, 0], -1)
-            if cnt > 0:
-                return True, cnt+1
-            else:
-                return True, 0
-    return False, 0
-
-
 # Open Camera
 camera = cv2.VideoCapture(0)
 camera.set(10, 200)
